cfa8a522f209a57c2bb77f0f8328456eb14f78ff
Ted - XPS <ted@dolk.ai>
2024-06-24 08:21:44 +0200

    Moved config to it's own folder
    Config will hold different types of config options

--- src/config/database.py
+++ src/config/database.py
@@ -1,10 +1,10 @@
 import os
 
 def get_postgres_uri():
-    host = os.environ.get("DB_HOST", "localhost")
+    host = os.environ.get("DB_HOST")
     port = os.environ.get("PORT")
     password = os.environ.get("DB_PASSWORD")
     user = os.environ.get("USER")
-    db_name = "order_allocation"
+    db_name = "product_warehouse"
 
     return f"postgresql:psycopg2://{user}:{password}@{host}:{port}/{db_name}"
